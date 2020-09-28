165805d478fcaf59b3b1f3f44a7f11e2145ab21d
Shalu6bajpai <bajpaishalu6@gmail.com>
2020-09-28 04:06:49 -0700

    Updates

---
+++ precache-manifest.2e19fde4727a2319496d21932db48b55.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467201f1fb0e91a903c3916066540474",
+    "url": "/Ipod-react/index.html"
+  },
+  {
+    "revision": "b47df6ccffbc7690a556",
+    "url": "/Ipod-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2c694396a3904141203c",
+    "url": "/Ipod-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b47df6ccffbc7690a556",
+    "url": "/Ipod-react/static/js/2.ece0a88d.chunk.js"
+  },
+  {
+    "revision": "3afc3f7b055b07aac8edf61359703488",
+    "url": "/Ipod-react/static/js/2.ece0a88d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c694396a3904141203c",
+    "url": "/Ipod-react/static/js/main.16e30d70.chunk.js"
+  },
+  {
+    "revision": "235812bca3a5fe440114",
+    "url": "/Ipod-react/static/js/runtime-main.b783f17c.js"
+  },
+  {
+    "revision": "9ba16188fa31aa053935b27151e30f78",
+    "url": "/Ipod-react/static/media/Senorita.9ba16188.mp3"
+  },
+  {
+    "revision": "57da9c7bf7069f67a5b67842f5b21607",
+    "url": "/Ipod-react/static/media/shalu.57da9c7b.jpg"
+  }
+]);
